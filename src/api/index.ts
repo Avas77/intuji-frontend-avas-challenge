a0d57c0271e7f868a2c6cfa80632d04411c8cddb
Avas <avas.bajracharya@renegadeinsurance.com>
2024-05-26 11:41:03 +0545

    feat: added search for supabase tables

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,12 @@
 import { IUsersResponse } from "../interfaces/IUsersResponse";
 import supabase from "../utils/supabase";
 
-export const getUsers = async (): Promise<IUsersResponse[] | null> => {
-  const { data } = await supabase.from("users").select();
+export const getUsers = async (
+  name: string
+): Promise<IUsersResponse[] | null> => {
+  const { data } = await supabase
+    .from("users")
+    .select()
+    .ilike("name", `%${name}%`);
   return data;
 };
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,12 +1,26 @@
 import search from "../../../public/icons/search.svg";
 import cross from "../../../public/icons/cross.svg";
 
-const SearchBar = () => {
+interface ISearchBarProps {
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SearchBar = ({ name, setName }: ISearchBarProps) => {
   return (
     <div className="flex justify-between items-center px-6 pt-6 pb-3 border border-gray">
       <img src={search} className="w-5 h-5" />
-      <input placeholder="Contacts" className="text-gray flex-1 ml-2" />
-      <img src={cross} className="w-4 h-4" />
+      <input
+        placeholder="Contacts"
+        className="flex-1 ml-2"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <img
+        src={cross}
+        className="w-4 h-4 cursor-pointer"
+        onClick={() => setName("")}
+      />
     </div>
   );
 };
